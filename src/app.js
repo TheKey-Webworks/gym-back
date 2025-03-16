c00f048b059c4ee616564740cb053edb012f04c5
fecka <fede123.fecka@gmail.com>
2025-03-16 15:23:47 -0300

    merge with dev branch

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser")
 const app = express()
 
 app.use(cookieParser())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "http://192.168.0.18:5173", credentials: true }))
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
